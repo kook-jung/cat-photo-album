908177ccf7f02c824fcb4ddf96cc75bc82b25a41
jung_kook <j.kook.email@gmail.com>
2021-10-12 20:19:06 +0900

    [feat] node 컴포넌트 개발

--- src/api/getRoot.ts
+++ src/api/getRoot.ts
@@ -1,15 +1,17 @@
 import axios from "axios";
 
 async function getRoot() {
-  const resultData = await axios
-    .get("https://zl3m4qq0l9.execute-api.ap-northeast-2.amazonaws.com/dev")
-    .then((response: any) => {
-      console.log("response: " + JSON.stringify(response));
-    })
-    .catch((error: Error) => {
-      console.log("error: " + error);
-    });
+  const resultData = await axios.get(
+    "https://zl3m4qq0l9.execute-api.ap-northeast-2.amazonaws.com/dev"
+  );
+  // .then((response: any) => {
+  //   console.log("response: " + JSON.stringify(response));
+  // })
+  // .catch((error: Error) => {
+  //   console.log("error: " + error);
+  // });
 
+  console.log("resultData: " + JSON.stringify(resultData));
   return resultData;
 }
 export default getRoot;
--- src/components/NodeComponent.tsx
+++ src/components/NodeComponent.tsx
@@ -1,11 +1,59 @@
+import React, { useState } from "react";
+import styled from "styled-components";
 import { getRoot } from "api";
 
-const NodeComponent = () => {
-  const nodeAtRoot = getRoot();
+import file from "img/file.png";
+import directory from "img/directory.png";
 
-  console.log("nodeAtRoot: " + JSON.stringify(nodeAtRoot));
-  console.log("nodeAtRoot: " + JSON.stringify(nodeAtRoot));
-  return <div>NodeComponent</div>;
+export type nodeInfo = {
+  id: number;
+  name: string;
+  type: string;
+  filePath: string | null;
+  parent: number | null;
+};
+
+const NodeComponent = () => {
+  const [nodeList, setNodeList] = useState<nodeInfo[]>([]);
+  React.useEffect(() => {
+    getRoot()
+      .then((response: any) => {
+        console.log("response: " + JSON.stringify(response));
+        setNodeList(response.data);
+      })
+      .catch((error: Error) => {
+        console.log("error: " + error);
+      });
+  }, []);
+  console.log("nodeList: " + JSON.stringify(nodeList));
+
+  const item = nodeList.map((node: any) => {
+    if (node.type === "DIRECTORY")
+      return (
+        <Node>
+          <Img src={directory} />
+          {node.name}
+        </Node>
+      );
+    if (node.type === "FILE")
+      return (
+        <div>
+          <Img src={file} />
+          {node.name}
+        </div>
+      );
+  });
+
+  return <div style={{ display: "flex" }}>{item}</div>;
 };
 
 export default NodeComponent;
+
+const Node = styled.div`
+  padding: 1rem;
+`;
+
+const Img = styled.img`
+  display: block;
+  width: 10vw;
+`;
